d610eed3f305471b72ab7b1919a2ca853f167d50
Ahmad Shahzad <shzdehmd@gmail.com>
2025-02-25 20:32:35 +0500

    Refactored the code to have separate components.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,35 @@
+const Header = (props) => {
+    return (
+        <>
+            <h1>{props.course}</h1>
+        </>
+    );
+};
+
+const Content = (props) => {
+    return (
+        <>
+            <p>
+                {props.coursework[0][0]} {props.coursework[0][1]}
+            </p>
+            <p>
+                {props.coursework[1][0]} {props.coursework[1][1]}
+            </p>
+            <p>
+                {props.coursework[2][0]} {props.coursework[2][1]}
+            </p>
+        </>
+    );
+};
+
+const Total = (props) => {
+    return (
+        <>
+            <p>Number of exercises {props.coursework[0][1] + props.coursework[1][1] + props.coursework[2][1]}</p>
+        </>
+    );
+};
+
 const App = () => {
     const course = 'Half Stack application development';
     const coursework = [
@@ -8,17 +40,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>{course}</h1>
-            <p>
-                {coursework[0][0]} {coursework[0][1]}
-            </p>
-            <p>
-                {coursework[1][0]} {coursework[1][1]}
-            </p>
-            <p>
-                {coursework[2][0]} {coursework[2][1]}
-            </p>
-            <p>Number of exercises {coursework[0][1] + coursework[1][1] + coursework[2][1]}</p>
+            <Header course={course} />
+            <Content coursework={coursework} />
+            <Total coursework={coursework} />
         </div>
     );
 };
