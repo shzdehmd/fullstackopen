1980d758fd5d3b3412cae1ee679708de01a5a444
Ahmad Shahzad <shzdehmd@gmail.com>
2025-02-25 20:22:41 +0500

    Created basic coursework app.

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const App = () => {
+    const course = 'Half Stack application development';
+    const coursework = [
+        ['Fundamentals of React', 10],
+        ['Using props to pass data', 7],
+        ['State of a component', 14],
+    ];
 
-function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div>
+            <h1>{course}</h1>
+            <p>
+                {coursework[0][0]} {coursework[0][1]}
+            </p>
+            <p>
+                {coursework[1][0]} {coursework[1][1]}
+            </p>
+            <p>
+                {coursework[2][0]} {coursework[2][1]}
+            </p>
+            <p>Number of exercises {coursework[0][1] + coursework[1][1] + coursework[2][1]}</p>
+        </div>
+    );
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export default App;
